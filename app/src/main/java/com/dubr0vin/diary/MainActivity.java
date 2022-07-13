605a14bc78df11a796f76f684099d92ac123f9f2
daniil <daniildubrovin@outlook.com>
2022-07-13 20:28:10 +0300

    init

---
+++ app/src/main/java/com/dubr0vin/diary/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dubr0vin.diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
